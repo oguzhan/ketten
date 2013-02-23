b6eb52f1dc6a2dd23616b1516757258156c1ca98
Paris Kolios <paris.k@gmail.com>
2013-02-23 17:54:33 +0100

    Add chains minimal test initial data

--- ketten/chains/tests.py
+++ ketten/chains/tests.py
@@ -1,11 +1,28 @@
+import datetime
+
+from django.contrib.auth.models import User
 from django.utils import unittest
 from django.test.client import Client
 
+from models import Chain
+
 
 class ChainTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        user = User.objects.create_user('test', 'test@test.com', 'test')
+        chain = {
+            'id': 1,
+            'title': 'Be cool',
+            'starting_date': datetime.datetime.now(),
+            'owner': user
+        }
+        chain = Chain(**chain)
+        chain.save()
+
     def test_chain_details(self):
         client = Client()
-        response = client.get('/chains/1')
+        response = client.get('/chains/1/')
         self.assertEqual(response.status_code, 200)
 
     def test_chain_list(self):
